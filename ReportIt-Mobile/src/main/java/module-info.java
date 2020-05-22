de81b0744a4a6f2de4aa62bf881f8ae9ae415d35
Dimitris Ioannou <43514376+dimitrisioan@users.noreply.github.com>
2020-05-22 18:16:27 +0300

    fix login,signup

--- ReportIt-Mobile/src/main/java/module-info.java
+++ ReportIt-Mobile/src/main/java/module-info.java
@@ -3,5 +3,7 @@ module org.ReportIt {
     requires javafx.fxml;
 
     opens org.ReportIt to javafx.fxml;
+    opens org.ReportIt.Controllers to javafx.fxml;
     exports org.ReportIt;
+    exports org.ReportIt.Controllers;
 }
--- ReportIt-Mobile/src/main/java/org/ReportIt/App.java
+++ ReportIt-Mobile/src/main/java/org/ReportIt/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"));
+        scene = new Scene(loadFXML("views/Login"));
         stage.setScene(scene);
         stage.show();
     }
@@ -27,6 +27,7 @@ public class App extends Application {
     }
 
     private static Parent loadFXML(String fxml) throws IOException {
+        System.out.println(App.class);
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
         return fxmlLoader.load();
     }
