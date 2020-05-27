32241295d9346b5f96a42320c1720c51bb2f2857
Evaggelos Soulintzis <esoulintzhs@gmail.com>
2020-05-27 21:16:10 +0300

    delete mobile app

--- ReportIt-Mobile/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module org.ReportIt {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens org.ReportIt to javafx.fxml;
-    opens org.ReportIt.Controllers to javafx.fxml;
-    exports org.ReportIt;
-    exports org.ReportIt.Controllers;
-}
--- ReportIt-Mobile/src/main/java/org/ReportIt/Controllers/SignupContoller.java
+++
@@ -1,4 +0,0 @@
-package org.ReportIt.Controllers;
-
-public class SignupContoller {
-}
