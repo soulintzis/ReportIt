57d6bf4500416c770f1c9d6be97e4570521806d5
Evaggelos Soulintzis <esoulintzhs@gmail.com>
2020-06-02 21:09:58 +0300

    Fix combo box'

--- ReportIt-Desktop/src/main/java/org/ReportIt/Controllers/Mobile/AddProblemController.java
+++ ReportIt-Desktop/src/main/java/org/ReportIt/Controllers/Mobile/AddProblemController.java
@@ -2,27 +2,34 @@ package org.ReportIt.Controllers.Mobile;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class AddProblemController implements Initializable {
     @FXML
-    private ComboBox<String> combobox;
+    private ComboBox<String> type;
 
-    ObservableList<String> list = FXCollections.observableArrayList(
+    @FXML
+    private ComboBox<String> danger_degree;
+
+    ObservableList<String> types = FXCollections.observableArrayList(
         "Φωτεινός σηματοδότης εκτός λειτουργίας","Πρόβλημα στο οδόστρωμα/πεζοδρόμιο","Διαρροή σωλήνα παροχής νερού","Άλλο"
     );
+
+    ObservableList<String> danger = FXCollections.observableArrayList(
+            "Φωτεινός σηματοδότης εκτός λειτουργίας","Πρόβλημα στο οδόστρωμα/πεζοδρόμιο","Διαρροή σωλήνα παροχής νερού","Άλλο"
+    );
+
 //    FXCollections.observableArrayList(week_days);
 
     @Override
     public void initialize(URL url, ResourceBundle rb){
-        combobox.setItems(list);
+        type.setItems(types);
+        danger_degree.setItems(danger);
     }
 
 
